ed7539b25c2c3cf1768ba8094ff02cb45ff17736
AndyAYu <andyhkgw@gmail.com>
2021-12-31 15:05:55 -0500

    added thunk and logger

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
 
-
-import LoginFormContainer from './session_form/login_form_container';
-import SignupFormContainer from './session_form/signup_form_container';
+import LogInFormContainer from './session_form/login_form_container';
+import SignUpFormContainer from './session_form/signup_form_container';
 
 const App = () => (
     <div>
@@ -12,9 +18,10 @@ const App = () => (
             <h1>Robinhat</h1>
             </Link>
         </header>
-        <Routes>
-            <AuthRoute />
-        </Routes>
+        <Switch>
+            <AuthRoute exact path="/login" component={LogInFormContainer} />
+            <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+        </Switch>
     </div>
 );
 
