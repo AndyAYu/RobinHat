9667cd2c6307bed99505aab1f98107b67f5130fc
AndyAYu <andyhkgw@gmail.com>
2021-12-31 13:41:33 -0500

    finished figvaper-cell

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do 
+    resource :user, only: [:create]
+  end
+  
+  root "static_pages#root"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
