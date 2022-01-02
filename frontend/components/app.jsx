49cbe849f0bf833b5a47e06286c1fa7b5f34f850
AndyAYu <andyhkgw@gmail.com>
2022-01-02 11:32:40 -0500

    added stylesheet

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -12,12 +12,14 @@ import LogInFormContainer from './session_form/login_form_container';
 import SignUpFormContainer from './session_form/signup_form_container';
 import HeaderContainer from './header/header_container';
 import { AuthRoute } from '../util/route_util';
+import logo from '../../app/assets/images/hat-logo.png';
 
 const App = () => (
     <div>
         <header>
             <Link to="/" className="header-link">
             <h1>Robinhat</h1>
+            <img src={logo} width="50" height="50" />
             </Link>
             <HeaderContainer />
         </header>
