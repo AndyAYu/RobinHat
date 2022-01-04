ebe38d7a4de90b482479547474a0038783ff1985
AndyAYu <andyhkgw@gmail.com>
2022-01-04 18:18:39 -0500

    css second section

--- frontend/components/secondbody/secondbody.jsx
+++ frontend/components/secondbody/secondbody.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
 const SecondBody = () => (
-    <section className="first-section">
-            <span className="fee-schedule">
-            See our fee schedule to learn more about cost.
-            </span>
-    </section>
+    <div className="secondBody">
+        <section className="first-section">
+                <span className="fee-schedule">
+                See our fee schedule to learn more about cost.
+                </span>
+        </section>
+        <div className="second-section">
+            <div>
+                <img src={window.balloonPhotoURL} />
+            </div>
+
+            <div className="second-section-text">
+                <p>Introducing IPO Access</p>
+                <p>
+                    Get in at the IPO price. Now you can become one of
+                    the first public
+                </p>
+                <p>
+                    investors in the upcoming IPOs.
+                </p>
+            </div>
+
+        </div>
+    </div>
 )
 
 export default SecondBody
