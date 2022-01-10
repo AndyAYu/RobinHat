c59b4090f94e3ffda684f110531ad82097bd8ef6
AndyAYu <andyhkgw@gmail.com>
2022-01-10 13:49:38 -0500

    added headerdropdown.jsx and still working on dynamic button

--- frontend/components/splashbody/chartbody/chartbody.jsx
+++ frontend/components/splashbody/chartbody/chartbody.jsx
@@ -57,26 +57,6 @@ const options = {
 // in LineChart-want to make path endpoint and Link.name more dynamic.
 // ${data.dataset.label} < wrong reference???
 
-// export const data = { 
-//     labels,
-//     datasets: [
-//         {
-//             id: 1,
-//             label: 'AMD',
-//             data: closeV1.reverse(),
-//             borderColor: 'rgb(255, 99, 132)',
-//             backgroundColor: 'rgba(255, 99, 132, 0.5)',
-//         },
-//         {
-//             id: 2,
-//             label: 'DMA',
-//             data:closeV2.reverse(),
-//             borderColor: 'rgb(1,1,1)',
-//             backgroundColor: 'rgba(1,1,1, 0.5)'
-//         },
-//     ],
-// };
-
 // for stocks on the right side >> button onclick invoke defined function??
 
 class LineChart extends React.Component{
@@ -87,66 +67,72 @@ class LineChart extends React.Component{
                 labels: [],
                 datasets: []
             },
-        };
-        this.amdFetch = this.amdFetch.bind(this);
+            stonks: ['AMD', 'AAPL', 'GOOGL', 'FB', 'NFLX'],
+        }
+        this.stockFetch = this.stockFetch.bind(this);
     };
 
-    amdFetch(){
-        fetch(`https://cloud.iexapis.com/stable/stock/amd/chart/1y?token=pk_3e9931bb69894a0695a654b8e9715d4c`)
-        .then(response => response.json())
-        .then(data => {
-            let obj = data;
-            let closeValues = obj.map((p) => p.close);
-            let closeDates = obj.map((d) => d.date);
-            let ticker = "AMD";
-            let newobj = {
-                labels: closeDates,
-                datasets: [
-                    {
-                        label: ticker,
-                        data: closeValues,
-                        borderColor: 'rgb(190, 220, 211)',
-                        backgroundColor: 'rgba(190,220,211, 0.5)'
-                    }
-                ],
-            };
-            this.setState({newobj})
-            // .then(() => console.log(newobj))
-        })
+
+    stockFetch(stock) {
+        // document.getElementById("amd-stock-button").addEventListener("click", function(){
+            fetch(`https://cloud.iexapis.com/stable/stock/${stock}/chart/1y?token=pk_3e9931bb69894a0695a654b8e9715d4c`)
+            .then(response => response.json())
+            .then(data => {
+                let obj = data;
+                let closeValues = obj.map((p) => p.close);
+                let closeDates = obj.map((d) => d.date);
+                let ticker = `${stock}`;
+                let newobj = {
+                    labels: closeDates,
+                    datasets: [
+                        {
+                            label: ticker,
+                            data: closeValues,
+                            borderColor: 'rgb(190, 220, 211)',
+                            backgroundColor: 'rgba(190,220,211, 0.5)'
+                        }
+                    ],
+                };
+                this.setState({newobj})
+                // .then(() => console.log(newobj))
+            })
+        // })
 }   
 
     render(){
     return (
         <div className="stockpage">
-        <div className="chart-box">
-            <h2>Current Price</h2>
-            <Line
-            data={this.state.newobj}
-            options={options}
-            height={400}
-            width={600}
-            />
-        </div>
-        <div className="watchlist">
-            <div className="watchlist-box">
-            <p>Watchlist</p>
-            <div className="watchlist-stocks">
-                <div>
-                    {/* <button onClick={this.amdFetch} id="amdstock">
-                        <p>
-                        AMD
-                        </p>
-                    </button> */}
-                <Link to={`/stock/AMD`} onClick={this.amdFetch}>AMD</Link>
-                <br />
-                {/* <Link to={`/stock/DMA`}>DMA</Link>  */}
+            <div className="chart-box">
+                <h2>Current Price</h2>
+                <Line
+                data={this.state.newobj}
+                options={options}
+                height={400}
+                width={600}
+                />
+            </div>
+            <div className="watchlist">
+                <div className="watchlist-box">
+                    <p>Watchlist</p>
+                    <div className="watchlist-stocks">
+                        <div>
+                            <Link to={`/stock/amd`} onClick={this.stockFetch} id="amd-stock-button" >{this.state.stonks[0]}</Link>
+                        </div>
+                        <div>
+                            <Link to={`/stock/aapl`} onClick={this.stockFetch}>{this.state.stonks[1]}</Link>
+                        </div>
+                        <div>
+                            <Link to={`/stock/googl`} onClick={this.stockFetch}>{this.state.stonks[2]}</Link>
+                        </div>
+                        <div>
+                            <Link to={`/stock/fb`} onClick={this.stockFetch}>{this.state.stonks[3]}</Link>
+                        </div>
+                        <br />
+                    </div>
                 </div>
-                <br /> 
             </div>
-            </div>
-        </div>
         
-    </div>
+        </div>
     )
 }
 }
