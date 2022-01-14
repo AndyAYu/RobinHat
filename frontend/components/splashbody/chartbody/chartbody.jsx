9c57deb3aec713e25941f9d940df55a14595ae22
AndyAYu <andyhkgw@gmail.com>
2022-01-13 21:39:57 -0500

    took away quotes in news body

--- frontend/components/splashbody/chartbody/chartbody.jsx
+++ frontend/components/splashbody/chartbody/chartbody.jsx
@@ -212,9 +212,9 @@ class LineChart extends React.Component{
                     </div>
                         <div className="news-articles">
                             <div>
-                            <p>"Is It Time To Sell Apple After Stock Pulls Back From All-Time Highs?"</p>
+                            <p>Is It Time To Sell Apple After Stock Pulls Back From All-Time Highs?</p>
                             <span>
-                                "Apple Inc (NASDAQ: AAPL ) briefly traded at all-time highs to start the year before pulling back with the overall markets. Having bounced off the lows over the past two days, is it time to head for the exits? A lot of growth and technology stocks are still trading \"at valuations that don''t make sense for their growth prospects,\" Gilman Hill Asset Management''s Jenny Harrington said Wednesday on CNBC''s \"Fast Money Halftime Report.\" Apple''s earnings  Full story available on Benzinga.com",
+                                Apple Inc (NASDAQ: AAPL ) briefly traded at all-time highs to start the year before pulling back with the overall markets. Having bounced off the lows over the past two days, is it time to head for the exits? A lot of growth and technology stocks are still trading \"at valuations that don''t make sense for their growth prospects,\" Gilman Hill Asset Management''s Jenny Harrington said Wednesday on CNBC''s \"Fast Money Halftime Report.\" Apple''s earnings
                             </span>
                             </div>
                         <a href="https://cloud.iexapis.com/v1/news/article/3so4uJVfpeUnLQ5izkVmPhSTv2xY1a6F50TEmxdn0yt4">
@@ -224,7 +224,7 @@ class LineChart extends React.Component{
 
                         <div className="news-articles">
                             <div>
-                            <p>"The Best Drone Stocks For Today  And The Future"</p>
+                            <p>The Best Drone Stocks For Today  And The Future</p>
                             <span>
                                 Drone stocks are on the rise as the military invests in new drone platforms and the FAA establishes rules for commercial drones,
                                 opening up the possibility of drone delivery services.
@@ -237,7 +237,7 @@ class LineChart extends React.Component{
 
                         <div className="news-articles">
                             <div>
-                            <p>"The Antitrust Case Against Facebook Draws Blood"</p>
+                            <p>The Antitrust Case Against Facebook Draws Blood</p>
                             <span>ON TUESDAY, FEDERAL judge James E. Boasberg ruled that the Federal Trade Commission’s effort to break up Facebook could move forward.
                                 The case itself is far from decided. But by blessing the FTC’s theory that a monopoly can harm consumers even when its product is free,
                                 the judge has signaled that Facebook—and other tech platforms—are not invincible.</span>    
