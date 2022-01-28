43df7514fdda03b9da13ff8dd12936e575ba1c52
AndyAYu <andyhkgw@gmail.com>
2022-01-28 09:25:24 -0500

    stock_show container made

--- db/migrate/20211230221858_create_users.rb
+++ db/migrate/20211230221858_create_users.rb
@@ -1,10 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :email, null: false
       t.string :session_token, null: false
       t.string :password_digest, null: false
-
+      t.float :balance, null: false
       t.timestamps
     end
     add_index :users, :email, unique: true
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,10 +1,11 @@
 import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS, REMOVE_SESSION_ERRORS } from "../actions/session_actions";
 
-export default (state = [], action) => {
+
+const sessionErrorsReducer = (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_SESSION_ERRORS:
-            return action.errors;
+            return action.errors ;
         case RECEIVE_CURRENT_USER:
             return [];
         case REMOVE_SESSION_ERRORS:
@@ -13,3 +14,5 @@ export default (state = [], action) => {
             return state;
     }
 };
+
+export default sessionErrorsReducer;
