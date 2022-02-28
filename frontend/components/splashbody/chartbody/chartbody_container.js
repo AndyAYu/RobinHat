f9856cb1c2e018e2f8e4efa36776718a54431af3
AndyAYu <andyhkgw@gmail.com>
2022-02-28 17:53:44 -0500

    changing balance

--- frontend/components/splashbody/chartbody/chartbody_container.js
+++ frontend/components/splashbody/chartbody/chartbody_container.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import LineChart from './chartbody';
 
 const mapStateToProps = ({entities,session},ownParams) => {
+    debugger
     return {
         balance: entities.users[(session.id)].balance,
         stocks: entities.users[(session.id)].stocks.map(e => e.ticker),
