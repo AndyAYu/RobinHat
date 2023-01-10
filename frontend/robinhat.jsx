f0befe99050106e80039a9b36901a18fa62f0ca7
AndyAYu <andyhkgw@gmail.com>
2023-01-10 12:40:57 -0600

    buildpack order change

--- frontend/robinhat.jsx
+++ frontend/robinhat.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-
+//import something
 document.addEventListener("DOMContentLoaded", () => {
     let store;
     if (window.currentUser) {
